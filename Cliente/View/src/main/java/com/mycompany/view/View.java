5f3a8ccd8f791e5ab32b0cd0f76d4f3f2fae6f50
MarioSalcedo207068 <mario.salcedo207068@potros.itson.edu.mx>
2024-04-15 23:30:47 -0700

    Initial commit
    Packages and folders created

---
+++ Cliente/View/src/main/java/com/mycompany/view/View.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.view;
+
+/**
+ *
+ * @author salce
+ */
+public class View {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Servidor/ViewModel/src/main/java/com/mycompany/viewmodel/ViewModel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.viewmodel;
+
+/**
+ *
+ * @author salce
+ */
+public class ViewModel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
